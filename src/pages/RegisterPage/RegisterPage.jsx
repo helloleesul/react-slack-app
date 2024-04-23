472885a737c86ce0b5c5aca660aa61f33b2cfd15
LEE SUL <suerish.e@gmail.com>
2024-04-24 04:50:23 +0900

    Design: 회원가입 페이지 UI

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("🚀 ~ RegisterPage ~ data:", data);
+  };
+
+  return (
+    <div className="auth-wrapper">
+      <div style={{ textAlign: "center" }}>
+        <h3>Register</h3>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <p>This email field is required</p>}
+
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          {...register("name", { required: true, maxLength: 10 })}
+        />
+        {errors.name && errors.name.type === "required" && (
+          <p>This name field is required</p>
+        )}
+        {errors.name && errors.name.type === "maxLength" && (
+          <p>Your input exceed maximum length</p>
+        )}
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          {...register("password", { required: true, minLength: 6 })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <p>This password field is required</p>
+        )}
+        {errors.password && errors.password.type === "minLength" && (
+          <p>Password must have at least 6 characters</p>
+        )}
+
+        <input type="submit" disabled={loading} />
+        <Link to={"/login"} style={{ color: "gray", textDecoration: "none" }}>
+          이미 회원이신가요?
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPage;
